5abf8e9dec9fab45a169b9a509333785b208f79e
300853516 <300853516@PR-A315-30>
2017-07-11 15:42:10 -0400

    Add superclass

---
+++ com123-Lesson09A/SuperHuman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+Name: Anupom Roy
+Date: 
+version: 0.1 Created the superhuman subClass
+*/
+namespace com123_Lesson09A
+{
+    /// <summary>
+    /// This is super Human subclasss
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // private fields
+
+        // Public properties
+        // constract-----------------
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+    }
+}
