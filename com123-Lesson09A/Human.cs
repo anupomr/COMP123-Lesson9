0644efe3dbf726f19800594c2c0e295359add120
300853516 <300853516@PR-A315-30>
2017-07-11 15:27:39 -0400

    Add Human Abstract Class

---
+++ com123-Lesson09A/Human.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+Name:
+Anupom
+Version: 0.1 -class created
+*/
+
+namespace com123_Lesson09A
+{
+    /// <summary>
+    /// this is Humen abstract cluss
+    /// </summary>
+    public abstract class Human
+    {
+        //Private Fields
+        private string _name;
+
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+        // constract-----------------
+        public Human()
+        {
+
+        }
+    }
+}
---
+++ com123-Lesson09A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com123_Lesson09A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
