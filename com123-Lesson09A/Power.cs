7d345bff2d418bb9195e289bc4ec430d950cb897
300853516 <300853516@PR-A315-30>
2017-07-11 15:46:30 -0400

    Power

---
+++ com123-Lesson09A/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+Name: Anupom Roy
+Date: 
+Des: New power datatype
+version: 0.1 Created the power subClass
+*/
+namespace com123_Lesson09A
+{
+    /// <summary>
+    /// The Power
+    /// </summary>
+    public class Power
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
